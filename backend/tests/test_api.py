f218ba1412eb3884cd72e9131693f92a1309ff9f
Кумова Ксения <k.kumova@vkteam.ru>
2025-05-05 00:50:00 +0300

    init

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,5 +1,5 @@
 import requests
 
 def test_products():
-    response = requests.get("http://localhost/api/products")
+    response = requests.get("http://backend:5000/api/products")
     assert response.status_code == 200
